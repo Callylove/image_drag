0dcb267c4898effd079652042e423926d6ebe062
Enyia Christian <enyiachristian12@gmail.com>
2023-10-22 14:23:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,69 +1,59 @@
-let files = [];
-let moveFiles = [];
-let button = document.querySelector(".top button");
-let form = document.querySelector("form");
-let container = document.querySelector(".container");
-
-let text = document.querySelector(".inner");
-let browse = document.querySelector("#select");
-let input = document.querySelector("form input");
-
+const files = [];
+const button = document.querySelector(".top button");
+const form = document.querySelector("form");
+const container = document.querySelector(".container");
+const text = document.querySelector(".inner");
+const browse = document.querySelector("#select");
+const input = document.querySelector("form input");
 
 // input field
 browse.addEventListener("click", () => input.click());
-input.addEventListener("change", ()=> {
-let file = input.files;
-for (let a of file){
-    if(files.every(e => e.name != a.name)) files.push(a); 
-}
-form.reset();
-showImages();
-
-})
-
+input.addEventListener("change", () => {
+  const newFiles = Array.from(input.files);
+  for (const file of newFiles) {
+    if (!files.some((existingFile) => existingFile.name === file.name)) {
+      files.push(file);
+    }
+  }
+  form.reset();
+  showImages();
+});
 
 const showImages = () => {
-    let images = '';
-    files.forEach((img, del) => {
-        images += ` <div class="image">
-        <img src="${URL.createObjectURL(img)}" alt="image">
-        <span onclick="delImages(${del})">&times;</span>
-    </div>`
-    })
-    container.innerHTML = images;
-    
-    
-}
-const delImages = index => {
-    files.splice(index, 1);
-    showImages();
-}
+  const images = files.map((img, del) => `
+    <div class="image">
+      <img src="${URL.createObjectURL(img)}" alt="image">
+      <span onclick="delImages(${del})">&times;</span>
+    </div>
+  `).join('');
+  container.innerHTML = images;
+};
+
+const delImages = (index) => {
+  files.splice(index, 1);
+  showImages();
+};
+
 // form field
-form.addEventListener("dragover", e => {
-    e.preventDefault();
-
-    form.classList.add("dragover");
-    text.innerHTML = "Drop images here";
+form.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  form.classList.add("dragover");
+  text.innerHTML = "Drop images here";
 });
-form.addEventListener("dragleave", e => {
-    e.preventDefault();
 
-    form.classList.remove("dragover");
-    text.innerHTML = `Drag & drop image here or <span id="select">Browse</span>`;
+form.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  form.classList.remove("dragover");
+  text.innerHTML = `Drag & drop image here or <span id="select">Browse</span>`;
 });
 
 form.addEventListener("drop", (e) => {
-    e.preventDefault();
-    let file = e.dataTransfer.files;
-    for (let a of file){
-        if(files.every(e => e.name != a.name)) files.push(a); 
+  e.preventDefault();
+  const newFiles = Array.from(e.dataTransfer.files);
+  for (const file of newFiles) {
+    if (!files.some((existingFile) => existingFile.name === file.name)) {
+      files.push(file);
     }
-    showImages();
-   
-    
-})
-
-
-
-
-
+  }
+  showImages();
+});
